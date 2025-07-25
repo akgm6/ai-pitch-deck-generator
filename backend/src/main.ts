3e334175a8e1ac6e610ba7cf00c82ea5fa3e893f
akgm6 <alexis.gordonmartin@bison.howard.edu>
2025-07-25 12:54:02 -0500

    Deployment updates

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN,
+    origin: (origin, callback) => {
+      const allowedOrigins = process.env.CORS_ORIGIN?.split(',') || [];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
